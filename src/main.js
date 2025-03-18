5f0cc441b83b9163033b7f91696a7eabece977da
lauraM84 <laura.mazza84@gmail.com>
2025-03-18 13:30:14 +0100

    classroom

--- src/main.js
+++ src/main.js
@@ -21,19 +21,20 @@ window.next = next
 
 function render(data) {
   const container = document.getElementById("app")
+  const pokeContainer = document.createElement("div")
+  pokeContainer.className = "poke-container"
 
   container.innerText = ''
 
   for (const pokemon of data) {
-    const nameContainer = document.createElement("p")
+    const nameContainer = document.createElement("span")
     const node = document.createTextNode(pokemon.name)
     const image = document.createElement("img")
     image.src = pokemon.img
 
     nameContainer.appendChild(node)
-
-    container.appendChild(nameContainer)
-    container.appendChild(image)
-
+    pokeContainer.appendChild(image)
+    pokeContainer.appendChild(nameContainer)
+    container.appendChild(pokeContainer)
   }
 }
